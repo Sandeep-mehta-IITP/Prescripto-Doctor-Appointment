0029dc23c89fea22b7d2a0b25612999fbb7ee0b8
Sandeep-mehta-IITP <sandeep797671@gmail.com>
2025-05-03 16:37:09 +0530

    Enhance login page with form fields and toggle between Admin and Doctor login states

--- admin/src/pages/Login.jsx
+++ admin/src/pages/Login.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import React, { useState } from "react";
+import { assets } from "../assets/assets_admin/assets.js";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [state, setState] = useState("Admin");
 
-export default Login
+  return (
+    <form className="min-h-[80vh] flex items-center">
+      <div className="flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-[#5E5E5E] text-sm shadow-lg">
+        <p className="text-2xl font-semibold m-auto">
+          <span className="text-primary"> {state} </span>
+          Login
+        </p>
+        <div className="w-full">
+          <p>Email</p>
+          <input
+            type="email"
+            className="border border-[#DADADA] rounded w-full p-2 mt-1"
+            required
+          />
+        </div>
+
+        <div className="w-full">
+          <p>Password</p>
+          <input
+            type="password"
+            className="border border-[#DADADA] rounded w-full p-2 mt-1"
+            required
+          />
+        </div>
+
+        <button className="bg-primary text-white w-full py-2 rounded-md text-base cursor-pointer">
+          Login
+        </button>
+        {state === "Admin" ? (
+          <p>
+            Doctor Login?{" "}
+            <span
+              className="text-primary underline cursor-pointer"
+              onClick={() => setState("Doctor")}
+            >
+              Click Here
+            </span>
+          </p>
+        ) : (
+          <p>
+            Admin Login?{" "}
+            <span
+              className="text-primary underline cursor-pointer"
+              onClick={() => setState("Admin")}
+            >
+              Click Here
+            </span>
+          </p>
+        )}
+      </div>
+    </form>
+  );
+};
+
+export default Login;
