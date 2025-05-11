99e8b2cce5b0570b12461de69a7b3c30748c4fd1
Sandeep-mehta-IITP <sandeep797671@gmail.com>
2025-05-11 17:19:32 +0530

    Add doctor-related pages and update sidebar: implement DoctorDashboard, DoctorProfile, and DoctorAppointment components; enhance Sidebar to include navigation for doctors

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,6 +10,9 @@ import AllAppointments from "./pages/Admin/AllAppointments";
 import AddDoctor from "./pages/Admin/AddDoctor";
 import DoctorsList from "./pages/Admin/DoctorsList";
 import { DoctorContext } from "./context/DoctorContext";
+import DoctorDashboard from "./pages/Doctor/DoctorDashboard";
+import DoctorProfile from "./pages/Doctor/DoctorProfile";
+import DoctorAppointment from "./pages/Doctor/DoctorAppointment";
 
 const App = () => {
   const { aToken } = useContext(AdminContext);
@@ -22,11 +25,20 @@ const App = () => {
       <div className="flex items-start">
         <Sidebar />
         <Routes>
+
+          {/*-----Admin Route------*/}
           <Route path="/" element={<></>} />
           <Route path="/admin-dashboard" element={<Dashboard />} />
           <Route path="/all-appointments" element={<AllAppointments />} />
           <Route path="/add-doctor" element={<AddDoctor />} />
+
+
+          {/*-----Doctor Route------*/}
           <Route path="/doctor-list" element={<DoctorsList />} />
+          <Route path="/doctor-dashboard" element={<DoctorDashboard />} />
+          <Route path="/doctor-profile" element={<DoctorProfile />} />
+          <Route path="/doctor-appointments" element={<DoctorAppointment />} />
+
         </Routes>
       </div>
     </div>
